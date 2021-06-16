39a9dee8242c2a52a918a27f3638fe1c10f35ae6
gao497290234 <70793286+gao497290234@users.noreply.github.com>
2021-06-16 20:44:21 +0800

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -4,7 +4,8 @@ echo
 echo                                       马花藤定制定制脚本      
 echo 
 echo --------------------------------------------------------------------------------------------------
-#操作选项数据
+
+
 options=(对接SSR 对接V2ray 删除SSR端口 删除V2ray端口 退出脚本)
 
 red='\033[0;31m'
@@ -12,7 +13,6 @@ green='\033[0;32m'
 yellow='\033[0;33m'
 plain='\033[0m'
 
-#检测docker函数
 function docker_check(){
 	docker -v > /dev/null
 	if [ $? -eq  0 ]; then
@@ -21,15 +21,8 @@ function docker_check(){
 		return 1
 	fi
 }
-#安装docker函数
-function docker_install()
-{
-	#echo -e "${green}检查运行环境......${plain}"
-	#echo
-	#docker -v > /dev/null
-    #if [ $? -eq  0 ]; then
-        #echo -e "${green}环境已部署${plain}"
-    #else
+
+function docker_install(){
 	   echo -e "${yellow}检测运行环境未部署！${plain}"
     	   echo -e "${green}正在部署运行环境...${plain}"
         curl -fsSL https://get.docker.com -o get-docker.sh
@@ -37,9 +30,8 @@ function docker_install()
         echo -e "${green}环境部署完成${plain}"
         echo 
         echo ------------------------------------------------------------------------------------------------
-    #fi
 }
-#ssr对接函数
+
 get_SSR_url(){
 	echo --------------------------------------------------------------------------------------------------
 	read -p "请输入面板地址："  URL
@@ -64,9 +56,10 @@ get_SSR_url(){
 	echo
 	read -p "回车确定对接....."
 	name="SSR-$port"
-        echo "$name" >> ssr_port.conf
+    echo "$name" >> ssr_port.conf
 	docker run -d --name=$name -e NODE_ID=$ID -e API_INTERFACE=modwebapi -e WEBAPI_URL=$URL -e SPEEDTEST=0 -e WEBAPI_TOKEN=$KEY --log-opt max-size=1000m --log-opt max-file=3 -p $port:$port/tcp -p $port:$port/udp --restart=always origined/ssr:latest
 }
+
 get_v2ray_url(){
 	echo ----------------------------------------------------------------------------------
 	read -p "请输入面板地址："  URL
@@ -91,6 +84,7 @@ get_v2ray_url(){
 	echo
 	read -p "回车确定对接.....ctrl+c取消......"
 	name="V2-$port"
+	echo "$name" >> v2_port.conf
 	docker run -d --name=$name \
 	-e speedtest=0 -e api_port=2333 -e downWithPanel=0 \
 	-e node_id=$ID -e sspanel_url="$URL" -e key="$KEY" -e TZ="Asia/Shanghai"  -p $port:$port/tcp -p $port:$port/udp \
@@ -98,21 +92,16 @@ get_v2ray_url(){
 	--restart=always \
 	origined/v2ray:0.1
 }
-#删除ssr
+
 function rm_ssr(){
 li=$(wc -l < ssr_port.conf)
 check=0
 if [[ "$li" -eq "$check" ]]; then
-     echo
+    echo
 	echo --------------------------
 	echo -e "${green} SSR节点为空 ${plain}"
 	echo -------------------------------------------------------------------------------------------------------
 else
-#echo $li
-#for ((i=1;i<=$li;i++ )); do
-#    hint="${options[$i-1]}"
-#    echo -e "${green}${i}${plain}) ${hint}"
-#	echo count
 	for line in `cat ssr_port.conf`
 	do
     		x=$(($x+1))
@@ -142,13 +131,51 @@ else
 	fi
 fi
 }
-#运行一次后台检测docker已安装？自动安装;跳过。
+
+function rm_v2(){
+li=$(wc -l < v2_port.conf)
+check=0
+if [[ "$li" -eq "$check" ]]; then
+     echo
+	echo --------------------------
+	echo -e "${green} v2ray节点为空 ${plain}"
+	echo -------------------------------------------------------------------------------------------------------
+else
+	for line in `cat v2_port.conf`
+	do
+    		x=$(($x+1))
+    		hint[x]=${line}
+    		echo
+		echo --------------------------
+    		echo -e "${green}${x}${plain}. ${hint[x]}"
+    		echo -------------------------------------------------------------------------------------------------------
+	done
+	read -p "输入你要删除的选项:" selected
+	for((a=1;a<=$li;a++)); do
+		if [ $a -eq $selected ]; then
+     		sed -i "${a}d" v2_port.conf
+     		docker stop ${hint[a]}
+     		docker rm -f ${hint[a]}
+     		echo
+     		echo -e "${green} 选择的节点已删除 ${plain}"
+     		echo -------------------------------------------------------------------------------------------------------
+			tj=0
+			break
+		else
+			tj=1
+		fi
+	done
+	if [ $tj==1 ]; then
+		echo "请输入要删除的端口选项而非端口号"
+	fi
+fi
+}
+
 docker_check
 if [ $? -eq 1 ]; then
 	docker_install
 fi
 
-#用户选项界面
 while true
 do
 echo  "你要做什么呢:"
@@ -165,12 +192,12 @@ case "${selected}" in
     ;;
     3) rm_ssr
     ;;
-    4) echo "执行函数4"
+    4) rm_v2
     ;;
     5) break
     ;;
     *)
-    echo -e "[${red}Error${plain}] Please only enter a number [1-4]"
+    echo -e "[${red}Error${plain}] 请输入[1-5]的数值"
     ;;
 esac
 done
