cf61a3222f5d472784d44f303d6b85922579dac2
gao497290234 <70793286+gao497290234@users.noreply.github.com>
2022-03-07 03:08:03 -0500

    Create relay.sh

---
+++ relay.sh
@@ -0,0 +1,34 @@
+# !/bin/bash
+echo ====================================================================================================
+echo
+echo                                       马花藤定制定制脚本      
+echo 
+echo ====================================================================================================
+# check root
+[[ $EUID -ne 0 ]] && echo -e "${red}错误：${plain} 必须使用root用户运行此脚本！\n" && exit 1
+
+
+# check os
+if [[ -f /etc/redhat-release ]]; then
+    release="centos"
+elif cat /etc/issue | grep -Eqi "debian"; then
+    release="debian"
+elif cat /etc/issue | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+elif cat /proc/version | grep -Eqi "debian"; then
+    release="debian"
+elif cat /proc/version | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+else
+    echo -e "${red}未检测到系统版本，请联系脚本作者！${plain}\n" && exit 1
+fi
+options=(对接节点 删除节点 退出脚本)
+
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
