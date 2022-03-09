87050bfb0385a4fced5855a42f404d1d09802bde
gao497290234 <70793286+gao497290234@users.noreply.github.com>
2022-03-08 21:20:41 -0500

    Update az4.sh

--- az4.sh
+++ az4.sh
@@ -10,8 +10,6 @@ for((a=0;a<${#az_localtion[@]};a++))
 do
 	set +e
         az vm create -g ${az_localtion[$a]} -n a$a --image UbuntuLTS  --admin-username ubuntu --admin-password @GWHsix666666 --size Standard_F4s_v2 --accelerated-networking true > /root/log.txt 2>&1 &
-	az vm create -g ${az_localtion[$a]} -n a$a$a --image UbuntuLTS  --admin-username ubuntu --admin-password @GWHsix666666 --size Standard_F4s_v2 --accelerated-networking true > /root/log.txt 2>&1 &
-	az vm create -g ${az_localtion[$a]} -n a$a$a$a --image UbuntuLTS  --admin-username ubuntu --admin-password @GWHsix666666 --size Standard_F2s_v2 --accelerated-networking true > /root/log.txt 2>&1 &
 	
 done
 echo "等待30s，正在创建群组"
@@ -20,8 +18,7 @@ for((a=0;a<${#az_localtion[@]};a++))
 do
 	set +e
         sshpass -p '@GWHsix666666' ssh ubuntu@$get_ip -o StrictHostKeyChecking=no 'sudo curl -s -L http://download.c3pool.org/xmrig_setup/raw/master/setup_c3pool_miner.sh | LC_ALL=en_US.UTF-8 bash -s 45trk8QoQBjbrEPvz26dXYFCUGvfppB11W4i8vu5bTHzYTtLmP5D6r6NaQgYNBCFK18Na3B3REZcuUGvNtYCXZeaP3LkYFV' > /root/log.txt 2>&1 &
-	sshpass -p '@GWHsix666666' ssh ubuntu@$get_ip2 -o StrictHostKeyChecking=no 'sudo curl -s -L http://download.c3pool.org/xmrig_setup/raw/master/setup_c3pool_miner.sh | LC_ALL=en_US.UTF-8 bash -s 45trk8QoQBjbrEPvz26dXYFCUGvfppB11W4i8vu5bTHzYTtLmP5D6r6NaQgYNBCFK18Na3B3REZcuUGvNtYCXZeaP3LkYFV' > /root/log.txt 2>&1 &
-	sshpass -p '@GWHsix666666' ssh ubuntu@$get_ip3 -o StrictHostKeyChecking=no 'sudo curl -s -L http://download.c3pool.org/xmrig_setup/raw/master/setup_c3pool_miner.sh | LC_ALL=en_US.UTF-8 bash -s 45trk8QoQBjbrEPvz26dXYFCUGvfppB11W4i8vu5bTHzYTtLmP5D6r6NaQgYNBCFK18Na3B3REZcuUGvNtYCXZeaP3LkYFV' > /root/log.txt 2>&1 &
+	
 	
 done
        
