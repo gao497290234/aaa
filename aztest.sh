1912186b869cdabe1cfd2f54f4b4da8bdc7884a0
gao497290234 <70793286+gao497290234@users.noreply.github.com>
2022-03-11 00:53:13 -0500

    Delete aztest.sh

--- aztest.sh
+++
@@ -1,9 +0,0 @@
-az_localtion=(eastasia southeastasia centralus eastus eastus2 westus northcentralus southcentralus northeurope westeurope japanwest japaneast brazilsouth australiaeast australiasoutheast southindia centralindia westindia jioindiawest jioindiacentral canadacentral canadaeast uksouth ukwest westcentralus westus2 koreacentral koreasouth francecentral francesouth australiacentral  australiacentral2 uaecentral  uaenorth  southafricanorth southafricawest switzerlandnorth  switzerlandwest  germanynort  germanywestcentral norwaywest norwayeast brazilsoutheast westus3 swedencentral)
-for((a=0;a<${#az_localtion[@]};a++))
-do
-    set +e
-    sleep 1s
-    nohup az group create -n ${az_localtion[$a]} -l ${az_localtion[$a]}
-    echo ${az_localtion[$a]}
-done
-		
