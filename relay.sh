247089919fe79a8f722bd5b9d6e8a1645b9b34ed
gao497290234 <70793286+gao497290234@users.noreply.github.com>
2022-03-23 22:40:30 -0400

    Update relay.sh

--- relay.sh
+++ relay.sh
@@ -82,7 +82,7 @@ function docking(){
         docker run -d --name tcp-$a -e Lport=$a -e Rport=$a -e Rhost=user$[a/1000%10]-$[a%1000].h2yun.xyz --network host --restart=always origined/ehco:1.0
 	echo 已部署tcp转发tcp-$a
 	docker run -d --name udp-$a -e LPORT=$a -e RPORT=$a -e HOST=user$[a/1000%10]-$[a%1000].h2yun.xyz --network host --restart=always origined/gudp:1.0
-	echo 已部署udp转发tcp-$a
+	echo 已部署udp转发udp-$a
 done
 }
 
