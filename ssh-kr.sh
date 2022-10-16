2fcdd54b8f4c3c3d39d9bb3fa022182f73d8d1ad
gao497290234 <70793286+gao497290234@users.noreply.github.com>
2022-10-16 13:02:29 +0800

    Update ssh-kr.sh

--- ssh-kr.sh
+++ ssh-kr.sh
@@ -3,26 +3,6 @@ red='\033[0;31m'
 green='\033[0;32m'
 yellow='\033[0;33m'
 plain='\033[0m'
-echo ====================================================================================================
-read -p "please input adb path of flie: " input_file
-echo ====================================================================================================
-echo "abs path of flie is $input_file(if that is false please input Ctrl+C!!!)"
-echo ====================================================================================================
-read -p "palesae ouput ads path of flie: " output_file
-echo ====================================================================================================
-echo "abs path of flie is $output_file(if that is false please input Ctrl+C!!!)"
-num=$(cat $input_file | wc -l)
-echo "num is $num"
-for((i=1;i<=$num;i++));  
-do 	
-	echo "starti=$i"
-	set +e
-	address=$(sed -n "$i, 1p" $file | awk -F, '{print $1;}')
-	username=$(sed -n "$i, 1p" $file | awk -F, '{print $2;}')
-	passwd=$(sed -n "$i, 1p" $file | awk -F, '{print $3;}')
-	install_l2tp $address $username $passwd
-	output $address
-done 
 install_l2tp(){
 		echo
 		echo -------------------------------
@@ -45,3 +25,24 @@ EOF
 	echo "have wrote $output_file"
 	echo -------------------
 }
+
+echo ====================================================================================================
+read -p "please input adb path of flie: " input_file
+echo ====================================================================================================
+echo "abs path of flie is $input_file(if that is false please input Ctrl+C!!!)"
+echo ====================================================================================================
+read -p "palesae ouput ads path of flie: " output_file
+echo ====================================================================================================
+echo "abs path of flie is $output_file(if that is false please input Ctrl+C!!!)"
+num=$(cat $input_file | wc -l)
+echo "num is $num"
+for((i=1;i<=$num;i++));  
+do 	
+	echo "starti=$i"
+	set +e
+	address=$(sed -n "$i, 1p" $file | awk -F, '{print $1;}')
+	username=$(sed -n "$i, 1p" $file | awk -F, '{print $2;}')
+	passwd=$(sed -n "$i, 1p" $file | awk -F, '{print $3;}')
+	install_l2tp $address $username $passwd
+	output $address
+done 
