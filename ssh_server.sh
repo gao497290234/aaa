69ec0df226c022cbc25937671879f529332d8825
gao497290234 <70793286+gao497290234@users.noreply.github.com>
2022-10-16 11:48:57 +0800

    Update ssh_server.sh

--- ssh_server.sh
+++ ssh_server.sh
@@ -17,24 +17,24 @@ do
 	address=$(sed -n "$i, 1p" $file | awk -F, '{print $1;}')
 	username=$(sed -n "$i, 1p" $file | awk -F, '{print $2;}')
 	passwd=$(sed -n "$i, 1p" $file | awk -F, '{print $3;}')
-	install_l2tp($address $username $passwd)
-	output($address)
+	install_l2tp $address $username $passwd
+	output $address
 done 
-install_l2tp(a u p){
+install_l2tp(){
 		echo
 		echo -------------------------------
 		echo "正在为$a安装l2tp服务，请稍等..."
 		echo -------------------------------
-		sshpass -p $p ssh $u@$a -o StrictHostKeyChecking=no 'wget http://141.164.59.56/install_l2tp.sh' >> /root/log.txt 2>&1 &
+		sshpass -p ${3} ssh ${2}@${1} -o StrictHostKeyChecking=no 'wget http://141.164.59.56/install_l2tp.sh' >> /root/log.txt 2>&1 &
 		sleep 10
 		echo
 		echo ------------
 		echo "l2tp已部署"
 		echo ------------
 }
-output(a){
+output(){
 	cat >> $output_file << EOF
-	$a,admin,123456
+	${1},admin,123456
 EOF
 	echo 
 	echo -------------------
