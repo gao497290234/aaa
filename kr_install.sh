d1752a56b6a759f45546f68de4ae541099d6b217
gao497290234 <70793286+gao497290234@users.noreply.github.com>
2022-10-18 20:29:04 +0800

    Update kr_install.sh

--- kr_install.sh
+++ kr_install.sh
@@ -3,21 +3,22 @@ red='\033[0;31m'
 green='\033[0;32m'
 yellow='\033[0;33m'
 plain='\033[0m'
+
 install_l2tp(){
 	echo
-	echo -------------------------------
-	echo "${yellow}正在为${1}安装l2tp服务，请稍等...${plain}"
-	echo -------------------------------
+	echo ###################################################
+	echo "正在清理配置,请稍后..."
+	echo ###################################################
+	echo "正在为${1}安装l2tp服务，请稍等..."
 	sshpass -p "${3}" ssh ${2}@${1} -o StrictHostKeyChecking=no 'rm -f install_l2tp.sh&&wget http://141.164.59.56/install_l2tp.sh&&chmod 777 install_l2tp.sh&&sh install_l2tp.sh' >> /root/log.txt 2>&1 &
     	sleep 20
     	sshpass -p "${3}" ssh ${2}@${1} -o StrictHostKeyChecking=no "rm -f /etc/ppp/chap-secrets&&echo > /etc/ppp/chap-secrets '#USERNAME  PROVIDER  PASSWORD  IPADDRESS'" >> /root/log.txt 2>&1 &
     	echo sshpass -p "${3}" ssh ${2}@${1} -o StrictHostKeyChecking=no "echo >> /etc/ppp/chap-secrets admin '*' ${4} '*' "
 	sshpass -p "${3}" ssh ${2}@${1} -o StrictHostKeyChecking=no "echo >> /etc/ppp/chap-secrets admin '*' ${4} '*' " >> /root/log.txt 2>&1 &
     	sshpass -p "${3}" ssh ${2}@${1} -o StrictHostKeyChecking=no "/etc/init.d/xl2tpd restart"
-	echo
-	echo ------------
-	echo "${green}l2tp已安装${plain}"
-	echo ------------
+	echo ###################################################
+	echo "l2tp已安装"
+	echo ###################################################
 }
 output(){
 	echo $output_file
@@ -25,24 +26,38 @@ output(){
 	${1},admin,${2}
 EOF
 	echo 
-	echo -------------------
-	echo "${green}已将搭建的数据写入 $output_file${plain}"
-	echo -------------------
+	echo ###################################################
+	echo "已将搭建的数据写入 $output_file"
+	echo ###################################################
+	echo 
+	sleep 2
+	clear
 }
 
 echo ====================================================================================================
-read -p "${yellow}请输入放置cvs文件的绝对路径: ${plain}" input_file
+echo
+read -p "请输入放置cvs文件的绝对路径: " input_file
+echo
 echo ====================================================================================================
-echo "${green}csv文件的路径为: $input_file(如果不对请Ctrl+C终止脚本!!!)${plain}"
+echo
+echo "csv文件的路径为: $input_file(如果不对请Ctrl+C终止脚本!!!)"
+echo
 echo ====================================================================================================
-read -p "${yellow}请输入输出文件的绝对路径: ${plain}" output_file
+echo
+read -p "请输入输出文件的绝对路径: " output_file
+echo
 echo ====================================================================================================
-echo "${green}输出文件的绝对路径为: $output_file(如果不对请Ctrl+C终止脚本!!!)${plain}"
+echo
+echo "输出文件的绝对路径为: $output_file(如果不对请Ctrl+C终止脚本!!!)"
+echo
+sleep 2
+clear
+echo#####################################################################################################
 num=$(cat $input_file | wc -l)
-echo "共有${yellow} $num  ${plain}条地址"
+echo "共有 $num 条地址"
 for((i=1;i<=$num;i++));  
 do 	
-	echo "${green}正在为 $i 搭建${plain}"
+	echo "正在为 $i 搭建"
 	set +e
 	address=$(sed -n "$i, 1p" $input_file | awk -F, '{print $1;}')
 	username=$(sed -n "$i, 1p" $input_file | awk -F, '{print $2;}')
