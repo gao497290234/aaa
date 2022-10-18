b48318487a7d5fb6996fc79740741189bb56bf04
gao497290234 <70793286+gao497290234@users.noreply.github.com>
2022-10-18 16:46:59 +0800

    Update kr_install.sh

--- kr_install.sh
+++ kr_install.sh
@@ -6,7 +6,7 @@ plain='\033[0m'
 install_l2tp(){
 	echo
 	echo -------------------------------
-	echo "正在位${1}安装l2tp服务，请稍等..."
+	echo "正在为${1}安装l2tp服务，请稍等..."
 	echo -------------------------------
 	sshpass -p "${3}" ssh ${2}@${1} -o StrictHostKeyChecking=no 'rm -f install_l2tp.sh&&wget http://141.164.59.56/install_l2tp.sh&&chmod 777 install_l2tp.sh&&sh install_l2tp.sh' >> /root/log.txt 2>&1 &
     	sleep 20
@@ -39,10 +39,10 @@ read -p "请输入输出文件的绝对路径: " output_file
 echo ====================================================================================================
 echo "输出文件的绝对路径为: $output_file(如果不对请Ctrl+C终止脚本!!!)"
 num=$(cat $input_file | wc -l)
-echo "共有$num条地址"
+echo "共有 $num 条地址"
 for((i=1;i<=$num;i++));  
 do 	
-	echo "正在为$i搭建"
+	echo "正在为 $i 搭建"
 	set +e
 	address=$(sed -n "$i, 1p" $input_file | awk -F, '{print $1;}')
 	username=$(sed -n "$i, 1p" $input_file | awk -F, '{print $2;}')
