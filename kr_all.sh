cf109a79d2d4ced6b02e86e937ace9204dd560f8
gao497290234 <70793286+gao497290234@users.noreply.github.com>
2022-10-26 17:33:36 +0800

    Create kr_all.sh

---
+++ kr_all.sh
@@ -0,0 +1,132 @@
+
+# !/bin/bash
+echo ====================================================================================================
+echo
+echo                                       马花藤定制定制脚本      
+echo 
+echo ====================================================================================================
+# check root
+#[[ $EUID -ne 0 ]] && echo -e "${red}错误：${plain} 必须使用root用户运行此脚本！\n" && exit 1
+
+
+
+options=(搭建L2TP 搭建S5)
+
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+function install_l2tp(){
+	sshpass -p "${3}" ssh ${2}@${1} -o StrictHostKeyChecking=no 'rm -f install_l2tp.sh&&wget http://141.164.59.56/install_l2tp.sh&&chmod 777 install_l2tp.sh&&sh install_l2tp.sh' >> /root/log.txt 2>&1 &
+	echo ###################################################
+	echo "正在检测${1}残留配置,请稍后..."
+	echo ###################################################
+	sleep 10
+	echo "已清理全部数据,正在为${1}安装l2tp服务，请稍等..."
+    	sleep 12
+    	sshpass -p "${3}" ssh ${2}@${1} -o StrictHostKeyChecking=no "rm -f /etc/ppp/chap-secrets&&echo > /etc/ppp/chap-secrets '#USERNAME  PROVIDER  PASSWORD  IPADDRESS'" >> /root/log.txt 2>&1 &
+	sleep 5
+	sshpass -p "${3}" ssh ${2}@${1} -o StrictHostKeyChecking=no "echo >> /etc/ppp/chap-secrets admin '*' ${4} '*' " >> /root/log.txt 2>&1 &
+	sleep 3
+    	sshpass -p "${3}" ssh ${2}@${1} -o StrictHostKeyChecking=no "/etc/init.d/xl2tpd restart"
+	echo ###################################################
+	echo "l2tp已安装"
+	echo ###################################################
+	sleep 1
+}
+function output_l2tp(){
+	cat >> $output_file << EOF
+	${1},admin,${2}
+EOF
+
+dajian_l2tp(){
+for((i=1;i<=$num;i++));  
+do 	
+	echo "正在为第 $i 台搭建"
+	set +e
+	address=$(sed -n "$i, 1p" $input_file | awk -F, '{print $1;}')
+	username=$(sed -n "$i, 1p" $input_file | awk -F, '{print $2;}')
+	passwd=$(sed -n "$i, 1p" $input_file | awk -F, '{print $3;}')
+    	uuid=$(cat "/proc/sys/kernel/random/uuid")
+	psd=${uuid: 0: 6}
+	install_l2tp $address $username $passwd $psd
+	output_l2tp $address $psd
+done 
+}
+function install_s5(){
+  sshpass -p "${3}" ssh ${2}@${1} -o StrictHostKeyChecking=no "/etc/init.d/s5 stop&&/etc/init.d/xl2tpd stop" >> /root/log.txt 2>&1 &
+	sshpass -p "${3}" ssh ${2}@${1} -o StrictHostKeyChecking=no "echo > /root/psd.txt ${4}" >> /root/log.txt 2>&1 &
+	sleep 2
+	sshpass -p "${3}" ssh ${2}@${1} -o StrictHostKeyChecking=no 'rm -f install_s5.sh&&wget http://141.164.59.56/install_s5.sh&&chmod 777 install_s5.sh&&sh install_s5.sh' >> /root/log.txt 2>&1 &
+	sleep 5
+	echo ####################################################
+	echo "s5代理安装完成"
+	echo ###################################################
+	sleep 1
+}
+function output_s5(){
+	cat >> $output_file << EOF
+	${1},10001,admin,${2}
+EOF
+	echo ###################################################
+	echo "已将搭建的数据写入 $output_file"
+	echo ###################################################
+	sleep 2
+	clear
+}
+function dj_s5(){
+  for((i=1;i<=$num;i++));  
+do 	
+	echo "正在为第 $i 台搭建s5"
+	set +e
+	address=$(sed -n "$i, 1p" $input_file | awk -F, '{print $1;}')
+	username=$(sed -n "$i, 1p" $input_file | awk -F, '{print $2;}')
+	passwd=$(sed -n "$i, 1p" $input_file | awk -F, '{print $3;}')
+  	uuid=$(cat "/proc/sys/kernel/random/uuid")
+	psd=${uuid: 0: 6}
+	install_s5 $address $username $passwd $psd
+	output_s5 $address $psd
+}
+echo ====================================================================================================
+echo
+read -p "请输入放置cvs文件的绝对路径: " input_file
+echo
+echo ====================================================================================================
+echo
+echo "csv文件的路径为: $input_file(如果不对请Ctrl+C终止脚本!!!)"
+echo
+echo ====================================================================================================
+echo
+read -p "请输入输出文件的绝对路径: " output_file
+echo
+echo ====================================================================================================
+echo
+echo "输出文件的绝对路径为: $output_file(如果不对请Ctrl+C终止脚本!!!)"
+sleep 2
+clear
+echo #####################################################################################################
+num=$(cat $input_file | wc -l)
+echo "共有  $num  条地址"
+echo
+echo
+echo
+while true
+do
+echo  "你要做什么呢:"
+for ((i=1;i<=${#options[@]};i++ )); do
+    hint="${options[$i-1]}"
+    echo -e "${green}${i}${plain}. ${hint}"
+done
+read -p "输入你想做的选项(默认选择1 ${options[0]}):" selected
+[ -z "${selected}" ] && selected="1"
+case "${selected}" in
+    1) dajian_l2tp
+    ;;
+    2) delete
+    ;;
+    *)
+    echo -e "[${red}Error${plain}] 请输入[1-2]的数值"
+    ;;
+esac
+done
