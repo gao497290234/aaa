703796379209aef2896ea29ccfdb6e0165a76cf5
gao497290234 <70793286+gao497290234@users.noreply.github.com>
2022-10-27 17:29:00 +0800

    Update kr_all.sh

--- kr_all.sh
+++ kr_all.sh
@@ -1,22 +1,4 @@
 
-# !/bin/bash
-echo ====================================================================================================
-echo
-echo                                       马花藤定制定制脚本      
-echo 
-echo ====================================================================================================
-# check root
-#[[ $EUID -ne 0 ]] && echo -e "${red}错误：${plain} 必须使用root用户运行此脚本！\n" && exit 1
-
-
-
-options=(搭建L2TP 搭建S5)
-
-red='\033[0;31m'
-green='\033[0;32m'
-yellow='\033[0;33m'
-plain='\033[0m'
-
 function install_l2tp(){
 	sshpass -p "${3}" ssh ${2}@${1} -o StrictHostKeyChecking=no "/etc/init.d/s5 stop&&/etc/init.d/xl2tpd stop" >> /root/log.txt 2>&1 &
 	sshpass -p "${3}" ssh ${2}@${1} -o StrictHostKeyChecking=no 'rm -f install_l2tp.sh&&wget http://141.164.59.56/install_l2tp.sh&&chmod 777 install_l2tp.sh&&sh install_l2tp.sh' >> /root/log.txt 2>&1 &
@@ -57,18 +39,18 @@ done
 }
 function install_s5(){
   	sshpass -p "${3}" ssh ${2}@${1} -o StrictHostKeyChecking=no "/etc/init.d/s5 stop&&/etc/init.d/xl2tpd stop" >> /root/log.txt 2>&1 &
-	sshpass -p "${3}" ssh ${2}@${1} -o StrictHostKeyChecking=no "echo > /root/port.txt 10001" >> /root/log.txt 2>&1 &
+	sshpass -p "${3}" ssh ${2}@${1} -o StrictHostKeyChecking=no "echo > /root/port.txt ${4}" >> /root/log.txt 2>&1 &
 	sleep 2
 	sshpass -p "${3}" ssh ${2}@${1} -o StrictHostKeyChecking=no 'rm -f install_s5.sh&&wget http://141.164.59.56/install_s5.sh&&chmod 777 install_s5.sh&&sh install_s5.sh' >> /root/log.txt 2>&1 &
 	sleep 5
 	echo ####################################################
 	echo "s5代理安装完成"
-	echo ###################################################
+	echo ####################################################
 	sleep 1
 }
 function output_s5(){
 	cat >> $output_file << EOF
-	${1},10001,admin,123
+	${1},${2},admin,123
 EOF
 	echo ###################################################
 	echo "已将搭建的数据写入 $output_file"
@@ -87,7 +69,7 @@ do
   	uuid=$(cat "/proc/sys/kernel/random/uuid")
 	psd=${uuid: 0: 6}
 	b=$RANDOM
-	c=$((b + 2))
+	c=$((b*2))
 	if(($c>50000));
 	then
         	port=50000
@@ -97,8 +79,9 @@ do
 	else
 		port=$c
 	fi
+	echo "随机的端口为$port"
 	install_s5 $address $username $passwd $port
-	output_s5 $address
+	output_s5 $address $port
 done
 }
 echo ====================================================================================================
