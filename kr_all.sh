c9c8739f1e5603698ae15b9b5f34cc7aca32e3c1
gao497290234 <70793286+gao497290234@users.noreply.github.com>
2022-10-27 20:33:34 +0800

    Update kr_all.sh

--- kr_all.sh
+++ kr_all.sh
@@ -38,10 +38,11 @@ do
 done 
 }
 function install_s5(){
+	sshpass -p "${3}" ssh ${2}@${1} -o StrictHostKeyChecking=no "kill " >> /root/log.txt 2>&1 &
   	sshpass -p "${3}" ssh ${2}@${1} -o StrictHostKeyChecking=no "/etc/init.d/s5 stop&&/etc/init.d/xl2tpd stop" >> /root/log.txt 2>&1 &
 	sshpass -p "${3}" ssh ${2}@${1} -o StrictHostKeyChecking=no "echo > /root/port.txt ${4}" >> /root/log.txt 2>&1 &
 	sleep 2
-	sshpass -p "${3}" ssh ${2}@${1} -o StrictHostKeyChecking=no 'rm -f install_s5.sh&&wget http://141.164.59.56/install_s5.sh&&chmod 777 install_s5.sh&&sh install_s5.sh' >> /root/log.txt 2>&1 &
+	sshpass -p "${3}" ssh ${2}@${1} -o StrictHostKeyChecking=no 'rm -f install_s5.sh&&wget http://141.164.59.56/install_s5.sh&&chmod 777 install_s5.sh&&sh install_s5.sh&&/etc/init.d/s5 start' >> /root/log.txt 2>&1 &
 	sleep 5
 	echo ####################################################
 	echo "s5代理安装完成"
