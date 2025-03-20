23608e84e6e19b796fa8135fcc1d8211d3b2416e
gao497290234 <70793286+gao497290234@users.noreply.github.com>
2025-03-20 11:01:52 +0800

    Update ubuntu-psd.sh

--- ubuntu-psd.sh
+++ ubuntu-psd.sh
@@ -2,4 +2,5 @@
 echo root:mylsix0.0 |sudo chpasswd root
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
-sudo reboot
+sudo rm -rf /etc/ssh/sshd_config.d/*
+sudo systemctl restart ssh
